266030a08cd1799000ba303e7234803594dbe44f
root <you@exampl.com>
2022-03-16 17:06:06 -0700

    update1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,12 @@
  * Return: Always 0 (success).
  */
 
-int main (void)
+int main(void)
 {
-	char str[]="_putchar";
+	char str[] = "_putchar";
 	int ch;
 
-	for(ch=0; ch<8; ++ch)
+	for( ch = 0; ch < 8; ++ch)
 		_putchar(str[ch]);
 	_putchar('\n');
 
